bf4c0eac293cc8f5f17b75ba74ab323a995879fa
CodeFlash101 <126455673+CodeFlash101@users.noreply.github.com>
2023-02-28 15:25:37 +0000

    Pending changes exported from your codespace

---
+++ .vscode/madlibs.py
@@ -0,0 +1,6 @@
+noun = input ("Noun: ")
+verb = input ("Verb: ")
+
+madlib = (f"Jerasulem is important for Muslims, Christians and Jews One day {noun} will conquer Jerusalem. They will {verb} it a land of peace.")
+
+print(madlib)
